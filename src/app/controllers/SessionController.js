5bccb23c60ce3290d75834de7bde575944688a09
Pedro Ernesto Perdoná Rech <p.perdona@gmail.com>
2019-07-01 22:19:29 -0300

    'clean'

--- src/app/controllers/SessionController.js
+++ src/app/controllers/SessionController.js
@@ -3,23 +3,21 @@ import jwt from 'jsonwebtoken';
 import User from '../models/User';
 import authConfig from '../../config/auth';
 
+const schemaForPost = Yup.object().shape({
+  email: Yup.string()
+    .email()
+    .required(),
+  password: Yup.string().required(),
+});
+
 class SessionController {
   async store(req, res) {
-    const schema = Yup.object().shape({
-      email: Yup.string()
-        .email()
-        .required(),
-      password: Yup.string().required(),
-    });
-
-    if (!(await schema.isValid(req.body))) {
+    if (!(await schemaForPost.isValid(req.body))) {
       return res.status(400).json({ error: 'Validation fails' });
     }
 
     const { email, password } = req.body;
-
     const user = await User.findOne({ where: { email } });
-
     if (!user) {
       return res.status(401).json({ error: 'User not found' });
     }
